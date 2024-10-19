3d3a347d2145ab5ad497dc8c491f761689c4d1bc
mhtkmr008 <147491778+mhtkmr008@users.noreply.github.com>
2024-10-19 19:43:26 +0530

    changed from http to https

--- src/Service/MovieService.js
+++ src/Service/MovieService.js
@@ -5,7 +5,7 @@ const API_KEY='c4e5c48f';
 const MovieService={
     async searchMovies(name)
     {
-        const url=`http://www.omdbapi.com/?s=${name}&apikey=${API_KEY}`;
+        const url=`https://www.omdbapi.com/?s=${name}&apikey=${API_KEY}`;
         try{
             const response=await axios.get(url);
             return response.data;
@@ -17,7 +17,7 @@ const MovieService={
 
     async getMovieDetails(imdbID)
     {
-        const url=`http://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`;
+        const url=`https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`;
         try{
             const response =await axios.get(url);
             return response.data;
